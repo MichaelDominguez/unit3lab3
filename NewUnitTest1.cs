924e9529451ba34a04f88c9b4547cccdee1313bb
MichaelDominguez <guestmichaeldominguez@gmail.com>
2023-02-17 11:11:57 -0500

    unit3lab3

---
+++ NewUnitTest1.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using Xunit;
+using MyNewRootLib;
+
+namespace MyNewRootLibTest
+{
+
+public class NewUnitTest1
+{
+    [Fact]
+	public void TestPowerRootOf4() {
+	
+		double a = 2; // arrange
+		double expected = 4;
+	
+		double actual = MyNewRootLib.Root.Power(a); // act
+ 
+		Assert.Equal(expected, actual); // assert
+	}	
+	[Fact]
+    public void TestPowerRootOf9() {
+    
+		double a = 3; // arrange
+		double expected = 9;
+	
+		double actual = MyNewRootLib.Root.Power(a); // act
+
+		Assert.Equal(expected, actual); // assert
+	} 
+}
+}
